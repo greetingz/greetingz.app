6a59a9e0c8c5f25e7a5026ebad7302973eff14f3
Mo Behairy <MoShaaban@users.noreply.github.com>
2022-05-28 01:19:31 -0400

    Update images.js

--- assets/images.js
+++ assets/images.js
@@ -2,27 +2,32 @@ const images = [
   {
     name: "happy birthday orbit",
     cid: "bafkreidj2dzduucyspjsce7uxpg2mynxlydobkd2jbjj6q3qgxc2zknwvq",
-    tags: ["birthday", "space"],
+    tags: [Birthday"],
   },
   {
     name: "happy birthday sunflower",
     cid: "bafkreicjcyu64debg4ozrw2mspv2qjgcb6fewb5eg44n2byv2quqqjs73u",
-    tags: ["birthday", "nature"],
+    tags: ["Birthday"],
   },
   {
     name: "happy birthday tree trunk",
     cid: "bafkreiatbn46gqt6ul5cqw2dthxe3h55wqhiqkawnjhimbah7l4sbopthy",
-    tags: ["birthday", "nature"],
+    tags: ["Birthday"],
   },
   {
     name: "ramadan kareem",
     cid: "bafkreifkdw4jew44afimw6rie6vqhjwsxwntuvdm3qvzochprw2xnv6hgy",
-    tags: ["ramadan"],
+    tags: ["Ramadan"],
   },
   {
     name: "ramadan mubarak",
     cid: "bafkreicwi53rocqomwuwbuzdih7g7agz2ru7vgkn6j5ajn6hgj3dxhy3cm",
-    tags: ["ramadan"],
+    tags: ["Ramadan"],
+  },
+  {
+    name: "Babies, the Stars of our skies",
+    cid: "bafkreidms4idt5cgpowcehzjr4tkvgfc7wli46hmf72q6ywnspkifiq4jm",
+    tags: ["Baby"],
   },
 ].map((img) => ({
   ...img,
